87f0989b74c96a165a048345f4412f85ba579d90
santimgb98 <santimgb98@email.com>
2025-05-01 22:58:16 +0200

    added changeAvailability method

--- src/Book.java
+++ src/Book.java
@@ -7,16 +7,16 @@ public class Book {
     public int year;
     public int allUnits;
     public boolean available;
-    public boolean availableFalse = false;
 
-    public Book (String isbn, String name, String author, int year , int allUnits, boolean available, boolean availableFalse){
+
+    public Book (String isbn, String name, String author, int year , int allUnits, boolean available){
         this.isbn = isbn;
         this.name = name;
         this.author = author;
         this.year = year;
         this.allUnits = allUnits;
         this.available = available;
-        this.availableFalse = availableFalse;
+
 
     }
 
@@ -29,50 +29,36 @@ public class Book {
     public String getAuthor() {
         return author;
     }
-
     public int getYear() {
         return year;
     }
-
     public int getAllUnits() {
         return allUnits;
     }
-
     public boolean isAvailable() {
         return available;
     }
 
-    public boolean isAvailableFalse() {
-        return availableFalse;
-    }
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public void setAuthor(String author) {
         this.author = author;
     }
-
     public void setYear(int year) {
         this.year = year;
     }
-
     public void setAllUnits(int allUnits) {
         this.allUnits = allUnits;
     }
-
     public void setAvailable(boolean available) {
         this.available = available;
     }
 
-    public void setAvailableFalse(boolean availableFalse) {
-        this.availableFalse = availableFalse;
-    }
 
     @Override
     public String toString(){
